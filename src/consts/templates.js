0dbf26e5d384be1ca463016f17e15b3e4e5c70d3
margalitb@gmail.com <margalitb@liveperson.com>
2019-01-28 20:30:59 +0200

    unit_tests_with_jest

--- src/consts/templates.js
+++ src/consts/templates.js
@@ -2,5 +2,5 @@ import { template } from '../utils/utils';
 
 export default {
     SUBTITLES_URL: template`https://${'domain'}/candidate-evaluation/video/upload/${'urlParams'}/${'videoPublicId'}.mp4`,
-    SUBTITLES_PARAMS: template`l_text:${'textStyle'}:${'text'},${'location'},y_80,so_${'startTime'},eo_${'endTime'}`,
+    SUBTITLES_PARAMS: template`l_text:${'textStyle'}:${'text'},${'textLocation'},y_80,so_${'startTime'},eo_${'endTime'}`,
 };
