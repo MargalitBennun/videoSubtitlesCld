044a33ee63295fec41046ea059eab33dd924d157
margalitb@gmail.com <margalitb@liveperson.com>
2019-01-28 19:49:29 +0200

    unit_tests_with_jest

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,41 +9,17 @@ function template(strings, ...keys) {
         return result.join('');
     });
 }
-const TIMEOUT = 10000;
-function loadJSON(url) {
+const loadJSON = (filepath) => {
     return new Promise((resolve, reject) => {
-
-        let callbackName = 'jsonpCallback';
-        let timeoutTrigger = window.setTimeout(function(){
-            window[callbackName] = Function.prototype;
-            reject(new Error('Timeout'));
-        }, TIMEOUT);
-
-        window[callbackName] = function(data){
-            window.clearTimeout(timeoutTrigger);
-            resolve(data);
-        };
-
-        let script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.src = url;
-
-        document.getElementsByTagName('head')[0].appendChild(script);
-
+        fetch(filepath, {
+            method: 'get'
+        }).then(function (response) {
+            resolve(response);
+        }).catch(function (err) {
+            reject(err);
+        });
     });
-}
-// const loadJSON = (filepath) => {
-//     return new Promise((resolve, reject) => {
-//         fetch(filepath, {
-//             method: 'get'
-//         }).then(function (response) {
-//             resolve(JSON.parse(response));
-//         }).catch(function (err) {
-//             reject(err);
-//         });
-//     });
-// };
+};
 
 
 export { template, loadJSON }
