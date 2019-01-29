b4aa6af2ed5611f3143f0fe7589d5844249b5b68
margalitb@gmail.com <margalitb@liveperson.com>
2019-01-29 10:04:40 +0200

    unit_tests_with_jest

--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,28 @@ const VideoSubtitles = (require('../dist/main')).VideoSubtitles;
 
 describe('Given addSubtitlesTest initialized', () => {
     let videoSubtitles;
+    const defaultStartTimeResult = 2.5;
+    const defaultEndTimeResult = 3.5;
+    const defaultTextLocation = 'g_north';
+    const defaultTextStyle = 'arial_40';
+    let defaultConf;
+    let defaultSubtitles;
+
     beforeEach(() => {
         videoSubtitles = new VideoSubtitles();
+        defaultSubtitles = [{
+            'start-timing': '0:2.5',
+            'end-timing': '0:3.5',
+            'text': 'hello',
+        }];
+        defaultConf= {
+            videoPublicId: 'myVideo',
+            textStyle: defaultTextStyle,
+            textLocation: defaultTextLocation,
+            subtitles: {
+                subtitles: defaultSubtitles,
+            },
+        };
     });
     afterEach(() => {
         videoSubtitles = null;
@@ -21,35 +41,17 @@ describe('Given addSubtitlesTest initialized', () => {
         expect(videoSubtitles._getSeconds(time)).toBe(expectedResult);
     });
     test('Then _convertJsonToUrlParams will return valid subtitles parameters', () => {
-        const parameters = '0:0.0';
-        const subtitles = [{
-            'start-timing': '0:2.5',
-            'end-timing': '0:3.5',
-            'text': 'hello',
-        }];
-        const location = 'g_north';
-        const textStyle = 'arial_40';
-        const startTimeResult = 2.5;
-        const endTimeResult = 3.5;
+        const expectedResult = `l_text:${defaultTextStyle}:${defaultSubtitles[0].text},${defaultTextLocation},y_80,so_${defaultStartTimeResult},eo_${defaultEndTimeResult}`;
 
-        const expectedResult = `l_text:${textStyle}:${subtitles[0].text},${location},y_80,so_${startTimeResult},eo_${endTimeResult}`;
-
-        expect(videoSubtitles._convertJsonToUrlParams(subtitles, textStyle, location)).toBe(expectedResult);
+        expect(videoSubtitles._convertJsonToUrlParams(defaultSubtitles, defaultTextStyle, defaultTextLocation)).toBe(expectedResult);
     });
     test('Then _convertJsonToUrlParams will escape the text value', () => {
-        const subtitles = [{
-            'start-timing': '0:2.5',
-            'end-timing': '0:3.5',
-            'text': 'it\'s a test',
-        }];
-        const location = 'g_north';
-        const textStyle = 'arial_40';
-        const startTimeResult = 2.5;
-        const endTimeResult = 3.5;
+        const text = 'it\'s a test'
+        defaultSubtitles[0].text = text;
 
-        const expectedResult = `l_text:${textStyle}:${encodeURIComponent(subtitles[0].text)},${location},y_80,so_${startTimeResult},eo_${endTimeResult}`;
+        const expectedResult = `l_text:${defaultTextStyle}:${encodeURIComponent(text)},${defaultTextLocation},y_80,so_${defaultStartTimeResult},eo_${defaultEndTimeResult}`;
 
-        expect(videoSubtitles._convertJsonToUrlParams(subtitles, textStyle, location)).toBe(expectedResult);
+        expect(videoSubtitles._convertJsonToUrlParams(defaultSubtitles, defaultTextStyle, defaultTextLocation)).toBe(expectedResult);
     });
     test('Then _convertJsonToUrlParams join few subtitles with \/ between subtitle to subtitle', () => {
         const subtitles = [{
@@ -61,93 +63,44 @@ describe('Given addSubtitlesTest initialized', () => {
             'end-timing': '0:5.5',
             'text': 'hello2',
         }];
-        const location = 'g_north';
-        const textStyle = 'arial_40';
         const startTimeResult0 = 2.5;
         const endTimeResult0 = 3.5;
         const startTimeResult1 = 4.5;
         const endTimeResult1 = 5.5;
 
-        const expectedResult = `l_text:${textStyle}:${encodeURIComponent(subtitles[0].text)},${location},y_80,so_${startTimeResult0},eo_${endTimeResult0}/` +
-        `l_text:${textStyle}:${subtitles[1].text},${location},y_80,so_${startTimeResult1},eo_${endTimeResult1}`;
+        const expectedResult = `l_text:${defaultTextStyle}:${encodeURIComponent(subtitles[0].text)},${defaultTextLocation},y_80,so_${startTimeResult0},eo_${endTimeResult0}/` +
+        `l_text:${defaultTextStyle}:${subtitles[1].text},${defaultTextLocation},y_80,so_${startTimeResult1},eo_${endTimeResult1}`;
 
-        expect(videoSubtitles._convertJsonToUrlParams(subtitles, textStyle, location)).toBe(expectedResult);
+        expect(videoSubtitles._convertJsonToUrlParams(subtitles, defaultTextStyle, defaultTextLocation)).toBe(expectedResult);
     });
     test('Then _getUrl returns a valid URL params with domain res.cloudinary.com', () => {
-        const subtitles = [{
-            'start-timing': '0:2.5',
-            'end-timing': '0:3.5',
-            'text': 'hello',
-        }];
-        const conf = {
-            videoPublicId: 'myVideo',
-            textLocation: 'g_north',
-            textStyle: 'arial_40',
-            subtitles: {
-                subtitles: subtitles,
-            },
-        };
-        const startTimeResult = 2.5;
-        const endTimeResult = 3.5;
-
         const expectedResult = `https://res.cloudinary.com/candidate-evaluation/video/upload/` +
-        `l_text:${conf.textStyle}:${subtitles[0].text},${conf.textLocation},y_80,so_${startTimeResult},eo_${endTimeResult}/${conf.videoPublicId}.mp4`;
+        `l_text:${defaultTextStyle}:${defaultConf.subtitles.subtitles[0].text},${defaultTextLocation},y_80,so_${defaultStartTimeResult},eo_${defaultEndTimeResult}/${defaultConf.videoPublicId}.mp4`;
 
-        expect(videoSubtitles._getUrl(conf)).toBe(expectedResult);
+        expect(videoSubtitles._getUrl(defaultConf)).toBe(expectedResult);
     });
     test('Then _getUrl returns a valid URL params with default text style (arial_60) when not provided', () => {
-        const subtitles = [{
-            'start-timing': '0:2.5',
-            'end-timing': '0:3.5',
-            'text': 'hello',
-        }];
-        const conf = {
-            videoPublicId: 'myVideo',
-            textLocation: 'g_north',
-            subtitles: {
-                subtitles: subtitles,
-            },
-        };
+        delete defaultConf.textStyle;
         const defaultTextStyle = 'arial_60';
-        const startTimeResult = 2.5;
-        const endTimeResult = 3.5;
 
         const expectedResult = `https://res.cloudinary.com/candidate-evaluation/video/upload/` +
-        `l_text:${defaultTextStyle}:${subtitles[0].text},${conf.textLocation},y_80,so_${startTimeResult},eo_${endTimeResult}/${conf.videoPublicId}.mp4`;
+        `l_text:${defaultTextStyle}:${defaultConf.subtitles.subtitles[0].text},${defaultTextLocation},y_80,so_${defaultStartTimeResult},eo_${defaultEndTimeResult}/${defaultConf.videoPublicId}.mp4`;
 
-        expect(videoSubtitles._getUrl(conf)).toBe(expectedResult);
+        expect(videoSubtitles._getUrl(defaultConf)).toBe(expectedResult);
     });
     test('Then _getUrl returns a valid URL params with default text location (g_south) when not provided', () => {
-        const subtitles = [{
-            'start-timing': '0:2.5',
-            'end-timing': '0:3.5',
-            'text': 'hello',
-        }];
-        const conf = {
-            videoPublicId: 'myVideo',
-            textStyle: 'arial_40',
-            subtitles: {
-                subtitles: subtitles,
-            },
-        };
+        delete defaultConf.textLocation;
         const defaultTextLocation = 'g_south';
-        const startTimeResult = 2.5;
-        const endTimeResult = 3.5;
 
         const expectedResult = `https://res.cloudinary.com/candidate-evaluation/video/upload/` +
-            `l_text:${conf.textStyle}:${subtitles[0].text},${defaultTextLocation},y_80,so_${startTimeResult},eo_${endTimeResult}/${conf.videoPublicId}.mp4`;
+            `l_text:${defaultTextStyle}:${defaultConf.subtitles.subtitles[0].text},${defaultTextLocation},y_80,so_${defaultStartTimeResult},eo_${defaultEndTimeResult}/${defaultConf.videoPublicId}.mp4`;
 
-        expect(videoSubtitles._getUrl(conf)).toBe(expectedResult);
+        expect(videoSubtitles._getUrl(defaultConf)).toBe(expectedResult);
     });
-    test('Then _readSubtitleJsonFile returns parsed valid JSON', done => {
-        const fileUrl = 'https://dl6.volafile.org/get/B4qmReje6B80y/subtitles.json';
-        videoSubtitles._readSubtitleJsonFile(fileUrl).then((data) => {
-            console.log('inside resolve');
-            expect(data).toBe('');
-            done();
-        }).catch((data) => {
-            console.log('inside reject');
-            done();
-        });
+    test('Then addSubtitlesToVideo returns valid cloudinary URL with Promise', () => {
+        const expectedResult = `https://res.cloudinary.com/candidate-evaluation/video/upload/` +
+            `l_text:${defaultTextStyle}:${defaultConf.subtitles.subtitles[0].text},${defaultTextLocation},y_80,so_${defaultStartTimeResult},eo_${defaultEndTimeResult}/${defaultConf.videoPublicId}.mp4`;
+
+        expect(videoSubtitles.addSubtitlesToVideo(defaultConf)).resolves.toEqual(expectedResult);
     });
 });
